8fc265446a449e5d90844ff550d27aee7f2f0b40
Juan Ignacio <juan.araya.h01@mail.pucv.cl>
2023-06-25 17:00:53 -0400

    Conexion bd
    Creacion de backend, instalacion express,mysql

---
+++ Backend/app.ts
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql=require("mysql");
+const app=express();
+//cors
+//const cors=require('cors');
+const bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+
+app.listen(3000, () => {
+    console.log(`Conectando al servidor http://localhost:${3000}`)
+})
+
+
+
+//app.use(cors());
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    port:  3306,
+    database : 'admacademico'
+  });
+
+  connection.connect(function(err:any) {
+    if (err) {
+      console.error('Error conectando a la DB ' + err.stack);
+      return;
+    }
+    console.log('Conexión establecida' + connection.threadId);
+  });
